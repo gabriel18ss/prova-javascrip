3369f50c2cbaac96cceedc0a72c5a8ada1da7c34
Gabriel Gonçalves <gabriel24f90@gmail.com>
2022-10-19 11:41:19 -0300

    prf

---
+++ site/src/paginas/exer10/index.js
@@ -0,0 +1,13 @@
+import './index.scss'
+
+export default function contarAsterisco(){
+    return(
+        <section>
+            <div>
+                <h1>
+                    oiuou
+                </h1>
+            </div>
+        </section>
+    )
+}
