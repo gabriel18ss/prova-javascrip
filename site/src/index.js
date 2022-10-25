ee415d9689d65520ff1c695aeeecb0fd2a98da4b
Gabriel Gonçalves <gabriel24f90@gmail.com>
2022-10-25 11:49:56 -0300

    QTD Alunos

--- site/src/index.js
+++ site/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import ContarI from './paginas/exer9';
 import Asterisco from './paginas/exer10';
+import Alunos from './paginas/qtdAlunos';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Asterisco />
+    <Alunos />
   </React.StrictMode>
 );
 
