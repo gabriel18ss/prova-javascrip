aded9a85760f46071a24152d33cbb425d7e2250f
Gustavo Gomes Costa <gustavogomes4002@gmail.com>
2022-11-20 11:47:13 -0300

    salario

--- site/src/index.js
+++ site/src/index.js
@@ -8,11 +8,12 @@ import Alunos from './paginas/qtdAlunos';
 import CalcularAçai from './paginas/ex1';
 import VereficarLibra from './paginas/ex2';
 import CalcularGramas from './paginas/ex3';
+import CalcularSalario from './paginas/ex4';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CalcularGramas />
+    <CalcularSalario />
   </React.StrictMode>
 );
 
