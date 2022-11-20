d84efbc8e8dcddb5281272b72dfe1e4238c9d32b
Gustavo Gomes Costa <gustavogomes4002@gmail.com>
2022-11-20 11:10:06 -0300

    libra

--- site/src/index.js
+++ site/src/index.js
@@ -6,11 +6,12 @@ import ContarI from './paginas/exer9';
 import Asterisco from './paginas/exer10';
 import Alunos from './paginas/qtdAlunos';
 import CalcularAçai from './paginas/ex1';
+import VereficarLibra from './paginas/ex2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CalcularAçai />
+    <VereficarLibra />
   </React.StrictMode>
 );
 
